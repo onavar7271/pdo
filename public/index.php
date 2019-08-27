dd5e6051599a1c02db0ec14324c045b847d20d72
Naper13 <onavar7271@my.sullivan.edu>
2019-08-27 14:16:02 -0700

    changes

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php include "templates/header.php"; ?>
 
 <ul>
+	
 	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
 	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
 	<li><a href="update.php"><strong>Update</strong></a> - edit a user</li>
